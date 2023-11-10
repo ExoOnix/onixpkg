c87ddb6b28a19943f1045a00275b986de6dd4056
ExoticOnix <72964425+ExoOnix@users.noreply.github.com>
2023-11-10 18:09:38 +0200

    Create opm.sh

---
+++ opm.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DIR="/usr/opm"
+
+# Function to install
+install() {
+    echo "Installing: $file"
+    if [ ! -f "$file" ]; then
+	echo "$file not found"
+	exit 1
+    fi
+    
+    if [ ! -d "$DIR" ]; then
+	mkdir "$DIR"
+    fi
+
+    cp $file $DIR
+    tar -xvf $file
+
+    cd $DIR/$(basename "$file" .opm)
+    echo "$(basename '$file' .opm)"
+    
+    cp -R ./* /
+
+}
+
+# Function to uninstall
+uninstall() {
+    echo "Uninstalling: $file"
+    # Add your uninstallation logic here
+}
+
+# Check for the correct number of arguments
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 [install|uninstall] <file>"
+    exit 1
+fi
+
+# Read the option and file
+option=$1
+file=$2
+
+# Case statement for options
+case $option in
+    install)
+        install
+        ;;
+    uninstall)
+        uninstall
+        ;;
+    *)
+        echo "Invalid option: $option. Please use 'install' or 'uninstall'."
+        exit 1
+        ;;
+esac
+
+# Print the variable
+echo "File: $file"
